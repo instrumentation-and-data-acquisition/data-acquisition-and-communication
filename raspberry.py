c6db0323f414350a6b69307ca936ffbc9e88501b
anaismartins <anaisabelmartins@tecnico.ulisboa.pt>
2022-04-01 13:19:35 +0100

    Revert "corrected y to check if the incoming value is int"
    This reverts commit 3de7c2dc4242626ceedcbce8fb0de591ef912120.

--- raspberry.py
+++ raspberry.py
@@ -22,28 +22,30 @@ class MainWindow(QtWidgets.QMainWindow,): #class made for creating the window an
         self.clearbutton.move(120,0)
 
         self.x = list(range(100))  #graph displays 100 points from 0 to 99 on x
-        self.y = [0] * 100  #and 100 points at 0 initially for y
-
-        self.option = '' #initializing option to use later to communicate with arduino
-
-        self.graphWidget.setBackground('k') #setting background color to black
-        pen = pg.mkPen(color=(0, 157, 224)) #setting the line color to the ist blue
+        self.y = [600] * 100  #and 100 points at 0 initially for y
         
-        self.data_line =  self.graphWidget.plot(self.x, self.y, pen=pen) #initializing line
-        self.timer = QtCore.QTimer() #initilizing timer
+        self.i = 0 #iteration variable for update_plot_data fucntion
+
+        self.option = ''
+
+        self.graphWidget.setBackground('w')
+
+        pen = pg.mkPen(color=(255, 0, 0))
+        
+        
+        
+        self.data_line =  self.graphWidget.plot(self.x, self.y, pen=pen)
+        self.timer = QtCore.QTimer()
         self.timer.setInterval(1)
-        self.timer.timeout.connect(self.update_plot_data) #while timer is connected, the update_plot_data function repeats
-        self.timer.start() #starts the timer
+        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.start()
         
     def initialQuestion(self):
-        '''
-        method for questioning the user to start the acquisition
-        needs to be a method for the class for self.option to be saved and used to pass to arduino
-        '''
+        
         print("Deseja começar a aquisição?\n")
-        self.option=input("Escreva start\n") #gets the input from the user
-        ser.write(self.option.encode('utf-8')) #writes the option to serial, which arduino will read
-        dummyRead = ser.readline().decode('utf-8').rstrip() #
+        self.option=input("Escreva start\n")
+        ser.write(self.option.encode('utf-8'))
+        dummyRead = ser.readline().decode('utf-8').rstrip()
         while self.option!='start':
             error = ser.readline().decode('utf-8').rstrip() # read só funciona tendo write
             print(error)
@@ -77,6 +79,10 @@ class MainWindow(QtWidgets.QMainWindow,): #class made for creating the window an
             
 
     def update_plot_data(self):
+        
+        #if (self.i == 0):
+         #   self.initialQuestion
+          #  ++self.i
 
         self.x = self.x[1:]  # Remove the first y element.
         self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
@@ -91,9 +97,8 @@ class MainWindow(QtWidgets.QMainWindow,): #class made for creating the window an
             #    print(aux)
             #   self.y[self.i] = int(aux)
             #else:
-        if (aux.isdigit()):
-            self.y = self.y[1:]
-            self.y.append(int(aux))
+        self.y = self.y[1:]
+        self.y.append(int(aux))
             #++self.i
         #line = ser.readline().decode('utf-8').rstrip()
         print(aux)
