0ce45ce4e1f683321d37a236df261bb36d0e0a8d
ines-moreira <80252303+ines-moreira@users.noreply.github.com>
2022-04-02 20:12:19 +0100

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -1,25 +1,20 @@
 
-int val = 0;
-int analogPin = A0;
-void setup()
-{
-  Serial.begin(9600);
+int val = 0; //variavel onde se guarda valor de tensao de porta analogica
+int analogPin = A0;  //porta analogica escolhida
+void setup() {
+  Serial.begin(9600); //baut rate de 9600 bits por segundo
 }
-void loop()
-{
-  if (Serial.available() > 0)
-  {
-    String data = Serial.readString();
-    if (data.equalsIgnoreCase("start"))
-    {
-      val = analogRead(analogPin);
-      Serial.print(val);
-      Serial.flush();
-    }
-    else
-    {
-      Serial.print("fatal error");
-      Serial.flush();
-    }
+void loop() {
+  if (Serial.available() > 0) {       //retorna o numero de caracteres disponiveis para leitura na porta serial
+    String data = Serial.readString(); //guarda na string data a string recebida pelo Raspberry Pi
+  if(data.equalsIgnoreCase("start")){  //compara a string data ao comando correto pretendido - "start".
+    val = analogRead(analogPin);       //caso o comando seja correto lê o valor da porta analógica 
+    Serial.print(val);                // e manda ao Raspberry Pi
+    Serial.flush();                  //o flush garante que todos os dados foram transmitidos antes de prosseguir com o programa
+  }
+  else{
+   Serial.print("FATAL ERROR");    // se o comando lido do Raspberry nao for o correto, o Arduino envia a mensagem "FATAL ERROR"
+   Serial.flush();
+  }
   }
 }
